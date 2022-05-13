1c1b27d509ceaf1a1d3152cd8bff9b8851860663
Carlos Romero <carlosonasisromero@gmail.com>
2022-05-12 22:08:31 -0400

    Evento agregar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import '../src/styles.css';
 import { TodoList } from "../src/js/class/todo-list.class";
-import { renderList } from "../src/js/components/component";
+import { renderList, renderTask } from "../src/js/components/component";
+import { Todo } from './js/class/todo.class';
 
+const newTodoInput = document.querySelector('.new-todo');
 const todoList = new TodoList();
 
-todoList.addTask('Aprender JavaScript')
-todoList.addTask('Aprender Node')
+newTodoInput.addEventListener('keyup', (event)=>{
 
+    const key = event.key;
+    let value = event.target.value;
 
-renderList(todoList._tasks);
+    if(key === "Enter" && value.length > 0 ){
+        
+        const ifExist = todoList._tasks.some( task =>{
+            return task.title === value;
+        })
+        if(ifExist){
+            return;
+        }
+        const task = new Todo(value);
+        todoList.addTask(value);
+        
+        event.target.value = '';
+        renderTask(task);
+    }
+})
--- src/js/components/component.js
+++ src/js/components/component.js
@@ -39,3 +39,37 @@ export const renderList = (tasks = [])=>{
     })
     todoList.append(liFragment);
 }
+export const renderTask = (task)=>{
+    const li = document.createElement('li');
+    const status = (task.completed)? "completed" : "false";
+    li.classList.add(status);
+    li.setAttribute("data-id",`${task.id}`);
+
+    const div = document.createElement('div');
+    div.classList.add('view');
+
+    const input = document.createElement('input');
+    input.classList.add('toggle');
+    input.setAttribute('type', 'checkbox');
+    if(task.completed){
+        input.setAttribute('checked', '');
+    }
+    div.append(input);
+
+    const label = document.createElement('label');
+    label.innerText = task.title;
+    div.append(label);
+
+    const button = document.createElement('button');
+    button.classList.add('destroy');
+    div.append(button);
+
+    const inputEdit = document.createElement('input');
+    inputEdit.classList.add('edit');
+    inputEdit.setAttribute('value', 'Create a TodoMVC template');
+
+
+    li.append(div);
+    li.append(inputEdit);
+    todoList.append(li);
+}
